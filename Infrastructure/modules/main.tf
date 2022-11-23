85fb308e3574c7239af9526b677574dabcacadaa
bdairshadi <bdairshadi@gmail.com>
2022-11-23 19:38:57 +0200

    Defined the webserver

--- Infrastructure/modules/main.tf
+++ Infrastructure/modules/main.tf
@@ -7,5 +7,12 @@ resource "aws_vpc" "main" {
 }
 
 module "flask_webserver" {
-    source = "./ec2" 
+    source = "./ec2"
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/16"
+    webserver_name = "Flask"
+    // Linux Amazon EC2  || ami-08c40ec9ead489470 -> Ubuntu EC2
+    ami = "ami-0b0dcb5067f052a63"
+    instance_type = 
 }
