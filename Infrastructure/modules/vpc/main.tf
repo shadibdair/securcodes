7d4493ebc111b0b2c8252d7bf42b958b79c95e7a
bdairshadi <bdairshadi@gmail.com>
2022-11-23 21:07:15 +0200

    Created module vpc

--- Infrastructure/modules/vpc/main.tf
+++ Infrastructure/modules/vpc/main.tf
@@ -1,18 +1,35 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+data "aws_region" "current" {}
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags                 = merge(var.tags, lookup(var.tags_for_resource, "aws_vpc", {}))
+}
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_default_route_table" "vpc" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+  tags                   = merge(var.tags, lookup(var.tags_for_resource, "aws_default_route_table", {}))
+}
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_vpc_dhcp_options" "vpc" {
+  count = var.enable_dhcp_options == true ? 1 : 0
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  domain_name = coalesce(
+    var.domain_name,
+    "${data.aws_region.current.name}.compute.internal",
+  )
+  domain_name_servers = var.domain_name_servers
+  tags                = merge(var.tags, lookup(var.tags_for_resource, "aws_vpc_dhcp_options", {}))
+}
+
+resource "aws_vpc_dhcp_options_association" "vpc_dhcp" {
+  count           = var.enable_dhcp_options == true ? 1 : 0
+  vpc_id          = aws_vpc.vpc.id
+  dhcp_options_id = aws_vpc_dhcp_options.vpc[0].id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = merge(var.tags, lookup(var.tags_for_resource, "aws_internet_gateway", {}))
 }
