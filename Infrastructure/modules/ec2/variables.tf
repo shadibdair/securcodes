2458e5ef3d3e4fe8c11abe3b347718e69a4a4c82
bdairshadi <bdairshadi@gmail.com>
2022-11-23 19:25:04 +0200

    Variables EC2 Modules

---
+++ Infrastructure/modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+    type = string
+    description = "VPC Id"
+}
+
+variable "cidr_block" {
+    type = string
+    description = "Subnet cidr block"
+}
+
+variable "webserver_name" {
+    type = string
+    description = "Name of the of the webserver"
+}
+
+variable "ami" {
+    type = string
+    description = "AMI to use on the webserver instance"
+}
+
+variable "instance_type" {
+    type = string
+    description = "Instance type"
+}
