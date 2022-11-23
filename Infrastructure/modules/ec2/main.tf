9f41f2b69e213aa01fe1df295de04e660c33fdc0
bdairshadi <bdairshadi@gmail.com>
2022-11-23 19:14:54 +0200

    EC2 module

---
+++ Infrastructure/modules/ec2/main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_version = ">= 0.12"
+}
+
+resource "aws_subnet" "webserver" {
+    vpc_id = var.vpc_id
+    cidr_block = var.cidr_block
+}
+
+resource "aws_instance" "webserver" {
+    ami = var.ami
+    iam_instance_profile = var.instance_type
+    subnet_id = aws_subnet.webserver.id
+
+    tags = {
+      "Name" = "${var.webserver_name} webserver"
+    }
+}
