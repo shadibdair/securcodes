29cc5cdc920db228266d752ad4f0e8136b28ed3f
bdairshadi <bdairshadi@gmail.com>
2022-11-25 13:42:54 +0200

    Improved the script jenkins

--- poc/jenkins-script.sh
+++ poc/jenkins-script.sh
@@ -5,6 +5,13 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y upgrade
 # Add required dependencies for the jenkins package
-sudo yum -y install java-11-openjdk
+sudo amazon-linux-extras install -y java-openjdk11
 sudo yum -y install jenkins
 sudo systemctl daemon-reload
+sudo systemctl start jenkins
+# EFS install
+sudo yum install -y amazon-efs-utils
+sudo mount /var/lib/jenkins/ /tmp/jenkins
+
+# Using the EFS mount
+#sudo mount -t efs -o tls fs-0a32d4009d2ed462c:/ /var/lib/jenkins
