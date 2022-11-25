22c8bbc7deeb0eed31d0e7c681029a7f092e787f
bdairshadi <bdairshadi@gmail.com>
2022-11-25 15:59:02 +0200

    Fixed the script

--- poc/jenkins-script.sh
+++ poc/jenkins-script.sh
@@ -11,7 +11,9 @@ sudo systemctl daemon-reload
 sudo systemctl start jenkins
 # EFS install
 sudo yum install -y amazon-efs-utils
-sudo mount /var/lib/jenkins/ /tmp/jenkins
+
 
 # Using the EFS mount
-#sudo mount -t efs -o tls fs-0a32d4009d2ed462c:/ /var/lib/jenkins
+sudo mount -t efs -o tls fs-0a32d4009d2ed462c:/ /var/lib/jenkins
+sudo systemctl start jenkins
+
