0804f16957f7e110593139a2b9507411dbcaa556
bdairshadi <bdairshadi@gmail.com>
2022-11-25 10:20:44 +0200

    Created a POC using terraform

---
+++ poc/ssh.tf
@@ -0,0 +1,14 @@
+// Generate the SSH keypair that we’ll use to configure the EC2 instance.
+// After that, write the private key to a local file and upload the public key to AWS
+resource "tls_private_key" "key" {
+algorithm = "RSA"
+}
+resource "local_file" "private_key" {
+filename          = "TEST.pem"
+sensitive_content = tls_private_key.key.private_key_pem
+file_permission   = "0400"
+}
+resource "aws_key_pair" "key_pair" {
+key_name   = "TEST"
+public_key = tls_private_key.key.public_key_openssh
+}
---
+++ poc/vpc.tf
@@ -0,0 +1,57 @@
+# Create VPC
+resource "aws_vpc" "vpc" {
+cidr_block = "${var.vpc-cidr}"
+instance_tenancy        = "default"
+enable_dns_hostnames    = true
+tags      = {
+Name    = "Test_VPC"
+}
+}
+
+# Create Internet Gateway and Attach it to VPC
+resource "aws_internet_gateway" "internet-gateway" {
+vpc_id    = aws_vpc.vpc.id
+tags = {
+Name    = "internet_gateway"
+}
+}
+
+# Create Public Subnet 1
+resource "aws_subnet" "public-subnet-1" {
+vpc_id                  = aws_vpc.vpc.id
+cidr_block              = "${var.Public_Subnet_1}"
+availability_zone       = "eu-central-1a"
+map_public_ip_on_launch = true
+tags      = {
+Name    = "public-subnet-1"
+}
+}
+
+# Create Route Table and Add Public Route
+resource "aws_route_table" "public-route-table" {
+vpc_id       = aws_vpc.vpc.id
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.internet-gateway.id
+}
+tags       = {
+Name     = "Public Route Table"
+}
+}
+
+# Associate Public Subnet 1 to "Public Route Table"
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+subnet_id           = aws_subnet.public-subnet-1.id
+route_table_id      = aws_route_table.public-route-table.id
+}
+
+# Create Private Subnet 1
+resource "aws_subnet" "private-subnet-1" {
+vpc_id                   = aws_vpc.vpc.id
+cidr_block               = "${var.Private_Subnet_1}"
+availability_zone        = "eu-central-1a"
+map_public_ip_on_launch  = false
+tags      = {
+Name    = "private-subnet-1"
+}
+}
