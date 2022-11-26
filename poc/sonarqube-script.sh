0fc809d5724ff6e6bd85e676dfe19d80803ba941
bdairshadi <bdairshadi@gmail.com>
2022-11-26 15:32:51 +0200

    Create a script to configure docker into ec2

---
+++ poc/sonarqube-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+# Install the most recent Docker Engine package.
+sudo amazon-linux-extras install docker
+# Start the Docker service.
+sudo service docker start
+# Add the ec2-user to the docker group so you can execute Docker commands without using sudo
+sudo usermod -a -G docker ec2-user
