5a15b5f522a5e03d1bf026cdf38f1bba9c1a18f4
bdairshadi <bdairshadi@gmail.com>
2022-11-29 19:37:31 +0200

    Improving the script

--- poc/sonarqube-script.sh
+++ poc/sonarqube-script.sh
@@ -5,5 +5,8 @@ sudo amazon-linux-extras install docker -y
 # Start the Docker service.
 sudo service docker start
 # Add the ec2-user to the docker group so you can execute Docker commands without using sudo
-sudo usermod -a -G docker ec2-user
-# 1
+sudo usermod -aG docker ec2-user
+# Pull image
+docker pull sonarqube:latest
+# Run sonarqube server
+docker run -d --name sonarqube -p 9000:9000 sonarqube:latest
