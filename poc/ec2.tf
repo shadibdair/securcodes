c3d2941f9a60b985253974902c057732ba23cdb9
bdairshadi <bdairshadi@gmail.com>
2022-11-29 19:05:21 +0200

    Created a new ec2 for sonarqube

--- poc/ec2.tf
+++ poc/ec2.tf
@@ -59,3 +59,52 @@ resource "aws_instance" "ec2_private" {
     "Name" = "EC2-Private"
   }
 }
+
+
+
+
+
+
+
+
+resource "aws_instance" "ec2_public-2" {
+  ami                         = "ami-0b0dcb5067f052a63"
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  //user_data                   = file("jenkins-script.sh")
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-PUBLIC"
+  }
+  # Copies the ssh key file to home dir
+  provisioner "file" {
+    source      = "./${var.key_name}.pem"
+    destination = "/home/ec2-user/${var.key_name}.pem"
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+  }
+  //chmod key 400 on EC2 instance
+  provisioner "remote-exec" {
+    inline = ["chmod 400 ~/${var.key_name}.pem"]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+  }
+
+  depends_on = [
+    aws_key_pair.key_pair
+  ]
+}
