848b74d499ee55222c5fd7f42c61456bdc5bf065
bdairshadi <bdairshadi@gmail.com>
2022-11-29 19:54:21 +0200

    Edited the outputs

--- poc/output.tf
+++ poc/output.tf
@@ -6,7 +6,10 @@ output "key_name" {
   value = aws_key_pair.key_pair.key_name
 }
 output "public_ip" {
-  value = aws_instance.ec2_public.public_ip
+  value = aws_instance.ec2_public-Jenkins.public_ip
+}
+output "public_ip-2" {
+  value = aws_instance.ec2_public-SonarQube.public_ip
 }
 output "private_ip" {
   value = aws_instance.ec2_private.private_ip
